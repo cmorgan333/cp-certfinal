5961d058a518874fdf31d8462ca90bb2918ca55d
cmorgan333 <78804785+cmorgan333@users.noreply.github.com>
2021-11-11 19:45:05 -0500

    recipes
    recipes

--- public/app/app.js
+++ public/app/app.js
@@ -5,6 +5,8 @@ var RECIPES = [
         recipeFullImg: "recipe-pizza.jpg",
         recipeBriefDescription:"Make pizza night super duper out of this world with homemade pizza. This recipe is supreme with vegetables and two types of meat. Yum!",
         recipeDescription:"Make pizza night super duper out of this world with homemade pizza. This recipe is supreme with vegetables and two types of meat. Yum!",
+        recipeTime:"1h 24min",
+        recipeServings:"4 servings",
         recipeIngredients: "1/4 batch pizza dough 2 tablespoons Last-Minute Pizza Sauce 10 slices pepperoni 1 cup cooked and crumbled Italian sausage 2 large mushrooms, sliced 1/4 bell pepper, sliced 1 tablespoon sliced black olives 1 cup shredded mozzarella cheese",
         recipeInstructions:"1. Preheat the oven to 475°. Spray pizza pan with nonstick cooking or line a baking sheet with parchment paper. 2. Flatten dough into a thin round and place on the pizza pan. 3. Spread pizza sauce over the dough. 4. Layer the toppings over the dough in the order listed . 5. Bake for 8 to 10 minutes or until the crust is crisp and the cheese melted and lightly browned.",
     },
@@ -14,6 +16,8 @@ var RECIPES = [
         recipeFullImg: "recipe-burger.jpg",
         recipeBriefDescription:"Sink your teeth into a delicious restaurant-style, hamburger recipe made from lean beef. Skip the prepackaged patties and take the extra time to craft up your own, and that little extra effort will be worth it.",
         recipeDescription:"Sink your teeth into a delicious restaurant-style, hamburger recipe made from lean beef. Skip the prepackaged patties and take the extra time to craft up your own, and that little extra effort will be worth it.Make pizza night super duper out of this world with homemade pizza. This recipe is supreme with vegetables and two types of meat. Yum!",
+        recipeTime:"30 min",
+        recipeServings:"4 servings",
         recipeIngredients: "1/4 batch pizza dough 2 tablespoons Last-Minute Pizza Sauce 10 slices pepperoni 1 cup cooked and crumbled Italian sausage 2 large mushrooms, sliced 1/4 bell pepper, sliced 1 tablespoon sliced black olives 1 cup shredded mozzarella cheese",
         recipeInstructions:"1. Preheat the oven to 475°. Spray pizza pan with nonstick cooking or line a baking sheet with parchment paper. 2. Flatten dough into a thin round and place on the pizza pan. 3. Spread pizza sauce over the dough. 4. Layer the toppings over the dough in the order listed . 5. Bake for 8 to 10 minutes or until the crust is crisp and the cheese melted and lightly browned.",
     },
@@ -23,6 +27,8 @@ var RECIPES = [
         recipeFullImg: "recipe-pilaf.jpg",
         recipeBriefDescription:"Chicken Biryani is a bold and flavorful Indian dish with crazy tender bites of chicken with bell peppers in a deliciously spiced and fragrant rice.",
         recipeDescription:"Chicken Biryani is a bold and flavorful Indian dish with crazy tender bites of chicken with bell peppers in a deliciously spiced and fragrant rice.",
+        recipeTime:"1h 15min",
+        recipeServings:"6 servings",
         recipeIngredients: "1/4 batch pizza dough 2 tablespoons Last-Minute Pizza Sauce 10 slices pepperoni 1 cup cooked and crumbled Italian sausage 2 large mushrooms, sliced 1/4 bell pepper, sliced 1 tablespoon sliced black olives 1 cup shredded mozzarella cheese",
         recipeInstructions:"1. Preheat the oven to 475°. Spray pizza pan with nonstick cooking or line a baking sheet with parchment paper. 2. Flatten dough into a thin round and place on the pizza pan. 3. Spread pizza sauce over the dough. 4. Layer the toppings over the dough in the order listed . 5. Bake for 8 to 10 minutes or until the crust is crisp and the cheese melted and lightly browned.",
     },
@@ -32,6 +38,8 @@ var RECIPES = [
         recipeFullImg: "recipe-chowmein.jpg",
         recipeBriefDescription:"A great Chow Mein comes down to the sauce - it takes more than just soy sauce and sugar! Jam packed with a surprising amount of hidden vegetables, customize this Chicken Chow Mein recipe using your protein of choice!",
         recipeDescription:"A great Chow Mein comes down to the sauce - it takes more than just soy sauce and sugar! Jam packed with a surprising amount of hidden vegetables, customize this Chicken Chow Mein recipe using your protein of choice!",
+        recipeTime:"20 min",
+        recipeServings:"4 servings",
         recipeIngredients: "1/4 batch pizza dough 2 tablespoons Last-Minute Pizza Sauce 10 slices pepperoni 1 cup cooked and crumbled Italian sausage 2 large mushrooms, sliced 1/4 bell pepper, sliced 1 tablespoon sliced black olives 1 cup shredded mozzarella cheese",
         recipeInstructions:"1. Preheat the oven to 475°. Spray pizza pan with nonstick cooking or line a baking sheet with parchment paper. 2. Flatten dough into a thin round and place on the pizza pan. 3. Spread pizza sauce over the dough. 4. Layer the toppings over the dough in the order listed . 5. Bake for 8 to 10 minutes or until the crust is crisp and the cheese melted and lightly browned.",
     },
@@ -50,8 +58,6 @@ function initURLListener(){
     $(window).on("hashchange", changeRoute);
     changeRoute();
 }
-
-
 
 $(document).ready(function() {
     initURLListener();
